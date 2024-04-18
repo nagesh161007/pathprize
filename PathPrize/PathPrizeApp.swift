344b10683f098429ad2f6e39d894dd0bb055a106
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2024-04-18 15:17:04 -0400

    Initial Commit

---
+++ PathPrize/PathPrizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathPrizeApp.swift
+//  PathPrize
+//
+//  Created by nagesh sairam on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PathPrizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
